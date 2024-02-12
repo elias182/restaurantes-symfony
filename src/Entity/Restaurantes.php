88cc38f5bc4e3863f556b77e4938328fd6c80349
Daniel Escobar <daniescomoli@gmail.com>
2024-02-12 14:14:46 +0100

    registro y login hecho, mejora en el header

--- src/Entity/Restaurantes.php
+++ src/Entity/Restaurantes.php
@@ -6,10 +6,12 @@ use App\Repository\RestaurantesRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: RestaurantesRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class Restaurantes implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -43,6 +45,9 @@ class Restaurantes implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\OneToMany(targetEntity: Pedidos::class, mappedBy: 'restaurante')]
     private Collection $pedidos;
+
+    #[ORM\Column(type: 'boolean')]
+    private $isVerified = false;
 
     public function __construct()
     {
@@ -193,6 +198,18 @@ class Restaurantes implements UserInterface, PasswordAuthenticatedUserInterface
                 $pedido->setRestaurante(null);
             }
         }
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): static
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
