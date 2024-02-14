9d44c8dcfdc6d6cdd605bee1a142221fa3fc7dd4
Daniel Escobar <daniescomoli@gmail.com>
2024-02-14 22:07:29 +0100

    mejora en vistas

---
+++ src/Security/UsuarioVerificado.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Security;
+use App\Entity\User as AppUser;
+use Symfony\Component\Security\Core\Exception\AccountExpiredException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException; 
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+class UsuarioVerificado implements UserCheckerInterface
+{
+public function checkPreAuth(UserInterface $user): void
+{
+if (!$user instanceof AppUser) {
+ return;
+}
+if (!$user->isVerified()) {
+// the message passed to this exception is meant to be displayed to the user
+throw new CustomUserMessageAccountStatusException('Todavía no se ha verificado tu cuenta.');
+}
+}
+public function checkPostAuth(UserInterface $user): void
+{
+if (!$user instanceof AppUser) {
+return;
+}
+if (!$user->isExpired()) {
+    throw new AccountExpiredException('...');
+    }
+
+}
+
+
+
+}
